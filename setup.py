5889adfe8db7d2094b7f9ccc3e71370f3e173b65
Akash Sonowal <work.akashsonowal@gmail.com>
2024-03-21 08:37:14 +0530

    update

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
 setup(
     name='ssd-fast',
     version='0.1',
     packages=find_packages(),
-    install_requires=None,
-    description='A simple, fast, pure PyTorch ssd inference engine',
+    install_requires=required,
+    description='A simple, fast ssd inference engine',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.dev/akashsonowal/ssd-fast-inference/',
