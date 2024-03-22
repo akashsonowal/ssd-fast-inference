4615ccf7ce577234b13021c23505a5e02983cc59
Akash Sonowal <work.akashsonowal@gmail.com>
2024-03-22 10:32:24 +0530

    update

--- inference.py
+++ inference.py
@@ -4,7 +4,14 @@ import queue
 
 
 def main():
+
+    num_devices = torch.cuda.device_count()
+    
     inference_threads = []
+
+    cuda_stream = torch.cuda.Stream(device)
+    while True:
+        next_image, image_event = image_queue.get()
 
 
 if __name__ == "__main__":
--- model.py
+++ model.py
@@ -1,5 +1,11 @@
+import torch 
+
 class SSD300:
     def __init__(self):
         pass 
     
-    def preprocess
+    def preprocess:
+        pass 
+    
+    def setup():
+        self._detector = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_ssd', model_math=model_precision).eval()
